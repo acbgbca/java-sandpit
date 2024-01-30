6108769bf6c530bc7d11f5e79122ef4bdff4b600
Andrew Barrow <60839662+acbgbca@users.noreply.github.com>
2024-01-30 09:20:32 +0000

    Cleaned pom, updated tests to JUnit 5

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # java-sandpit
+
 Simple base Java image. For helping with HackerRank and similar sites
+
+# Commands
+
+```mvn exec:java``` - Runs the Base class
+
+```mvn test``` - Runs the tests
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,36 +1,35 @@
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class BaseTest 
-    extends TestCase
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BaseTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public BaseTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( BaseTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
+    @DisplayName("Hard coded test")
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Parameterized test")
+    @ValueSource(strings = {"One", "two"})
+    public void parameterizedTest(String value)
+    {
+        assertNotNull( value );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Parameterized test")
+    @CsvSource({"One,One", "two,two"})
+    public void parameterizedTest(String input, String expected)
+    {
+        assertEquals(expected, input);
     }
 }
